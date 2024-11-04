c94b79d3a15ef356be5d4440e9d01803d092c698
Medbiocode <151658586+Medbiocode@users.noreply.github.com>
2024-11-04 21:10:37 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.startpage.com/";//your website url
+  let target = "https://www.startpage.com/do/mypage.pl?prfe=47ca066624588e7687fc14263b2542df506324f2f55bb8b4424a0ef2ee192df198640083459dbffdfaac26f6de572126435cbd50a5554813148fce4a76b6356f6813ed5a7bbeff6291214f8b";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
